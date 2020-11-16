033cd5e6d9b91520e457214aa95547959211ce85
Brett Needham <brettneedham88@gmail.com>
2020-11-16 16:43:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,4 @@ def using_concat(my_favorite_things, more_favs)
   @my_favorite_things.concat(["sports cars", "flatiron school"])
 end
 
-def using_insert()
+def using_insert(@list_of_programming_languages, )
